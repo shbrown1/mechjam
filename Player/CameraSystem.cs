10dd84fff4531155979e54641c27609fe1ec398a
Shaun <shaun.p.brown1@gmail.com>
2023-07-14 23:35:26 -0400

    camera loosened

--- Player/CameraSystem.cs
+++ Player/CameraSystem.cs
@@ -27,11 +27,11 @@ public class CameraSystem : Camera
     {
         if (_cameraState == CameraState.FollowingPlayer)
         {
-            var _cameraMovementLerpSpeed = 50f;
+            var _cameraMovementLerpSpeed = 20f;
             var cameraHolderTranslation = _playerCameraHolder.GlobalTranslation;
             GlobalTranslation = GlobalTranslation.LinearInterpolate(cameraHolderTranslation, Mathf.Clamp(delta * _cameraMovementLerpSpeed, 0, 1));
 
-            var _cameraRotationLerpSpeed = 100f;
+            var _cameraRotationLerpSpeed = 20f;
             var cameraHolderBasis = new Basis(_playerCameraHolder.GlobalRotation);
             var currentBasis = new Basis(GlobalRotation);
             currentBasis = currentBasis.Slerp(cameraHolderBasis, Mathf.Clamp(delta * _cameraRotationLerpSpeed, 0, 1));
